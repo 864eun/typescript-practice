df993ed5408afe8c794ce26d3f05b4fb9b737ecf
eun <eun00864@gmail.com>
2025-04-28 20:07:30 +0900

    Add:타입스크립트 배열 타입

---
+++ section2/src/chapter2.ts
@@ -0,0 +1,8 @@
+//배열
+let numArr: number[] = [1, 2, 3];
+let strArr: string[] = ["hello", "im", "winterlood"];
+
+let boolArr: Array<boolean> = [true, false, true];
+
+// 두가지 이상 타입을 담고 있는 배열
+let multiArr: (number|string)[] = [1, "hello"];
