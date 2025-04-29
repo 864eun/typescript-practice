c18b500e071647fe251d2b523a5378e33c8dfa6a
eun <eun00864@gmail.com>
2025-04-29 16:12:29 +0900

    Add:section3 설정

--- section2/src/chapter4.ts
+++ section2/src/chapter4.ts
@@ -26,4 +26,22 @@ type User = {
     bio: "안녕하세요",
     location: "부천시",
   };
-  
+
+//인덱스 시그니처(Index Signature
+//키와 값의 타입을 정의하여 속성들을 모두 명시할 필요가 없도록 하는 것
+type CountryNumberCodes = {
+    [key: string]: number;
+  };
+
+//만약 필수적으로 포함해야 할 값이 있다면 아래와 같이 정의할 수 있다.
+type CountryNumberCodes1 = {
+    [key: string]: number;
+    Korea: number;
+  };
+
+//단 직접 추가한 속성의 value타입은 인덱스 시그니쳐 value타입과 일치하거나 호환이 가능해야한다.
+//아래와 같이 인덱스 시그니쳐 value타입이 number인 경우 직접 추가한 속성의 value타입이 string이라면 오류 발생
+// type CountryNumberCodes3 = {
+//     [key: string]: number;
+//     Korea: string; // 오류!
+//   };
