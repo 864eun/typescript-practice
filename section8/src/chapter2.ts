00904a9d2f2b6ac992d650be87133ca5b87eb9ca
eun <eun00864@gmail.com>
2025-05-02 17:56:38 +0900

    Add:타입스크립트 keyof & typeof 연산자

---
+++ section8/src/chapter2.ts
@@ -0,0 +1,41 @@
+//keyof & typeof 연산자
+//1. keyof
+interface Person {
+  name: string;
+  age: number;
+  location: string; // 추가
+}
+
+//v1 -> 문제점 : Person 타입에 프로퍼티가 추가되면 수정이 필요하다.
+function getPropertyKey1(person: Person, key: "name" | "age" | "location") {
+  return person[key];
+}
+
+//v2 -> keyof Person을 사용해서 추가 수정이 필요없게 함
+function getPropertyKey2(person: Person, key: keyof Person) {
+  return person[key];
+}
+
+const person: Person = {
+  name: "홍길동",
+  age: 27,
+  location: "서울",
+};
+
+//typeof
+interface Person1 {
+  name: string;
+  age: number;
+}
+
+function getPropertyKey(person: Person1, key: keyof typeof person) {
+  return person[key];
+}
+
+const person2: Person1 = {
+  name: "이정환",
+  age: 27,
+};
+
+getPropertyKey(person, "name"); // "이정환"
+getPropertyKey(person, "age"); // 27
